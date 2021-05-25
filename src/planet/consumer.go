7b5beb3eca52a69a6723fab1597df12ad49c3cc5
antonio-decaro <antonio.decaro99@outlook.it>
2021-05-25 18:15:12 +0200

    skeleton consumer spatial probe

--- src/planet/consumer.go
+++ src/planet/consumer.go
@@ -22,8 +22,9 @@ type Logger struct {
 	queue amqp.Queue
 }
 
-type ProbeData struct {
+type SpaceProbeData struct {
 	Name        string
+	Coordinate  [2]float64
 	Distance    float32
 	Mass        int32
 	Radius      float32
@@ -32,7 +33,7 @@ type ProbeData struct {
 }
 
 func Handler(context *nuclio.Context, event nuclio.Event) (interface{}, error) {
-	logger, err := InitLogger()
+	logger, err := initLogger()
 	if err != nil {
 		context.Logger.Error("Error: %s", err)
 		panic(err)
@@ -44,22 +45,51 @@ func Handler(context *nuclio.Context, event nuclio.Event) (interface{}, error) {
 		body := event.GetBody()
 		logger.log("Body content: " + string(body))
 
-		var p ProbeData
-		json.Unmarshal(body, &p)
+		var data SpaceProbeData
+		json.Unmarshal(body, &data)
 
-		context.Logger.Info(p)
-		logger.log(fmt.Sprint(p))
+		ch := make(chan error)
+
+		go func(ch chan<- error) {
+			var err error
+
+			if isHabitable(data) {
+				logger.log(fmt.Sprintf("Planet %+v is potentially habiatble!", data))
+				err = sendPlanetProbe(data)
+			} else {
+				logger.log(fmt.Sprintf("Planet %+v is not habitable", data))
+			}
+			ch <- err
+		}(ch)
+
+		persistProbeData(data)
+
+		// waiting for classifying operation to complete
+		<-ch
 
 		return nil, nil
 	}
 
 	return nuclio.Response{
-		StatusCode:  http.StatusOK,
-		ContentType: "application/json",
+		StatusCode:  http.StatusForbidden,
+		ContentType: "text/plain",
+		Body:        []byte("This is not a Web API"),
 	}, nil
 }
 
-func InitLogger() (*Logger, error) {
+func persistProbeData(data SpaceProbeData) error {
+	return nil
+}
+
+func sendPlanetProbe(data SpaceProbeData) error {
+	return nil
+}
+
+func isHabitable(data SpaceProbeData) bool {
+	return true
+}
+
+func initLogger() (*Logger, error) {
 	username := os.Getenv(USERNAME_ENV)
 	password := os.Getenv(PASSWORD_ENV)
 	ip := os.Getenv(IP_ENV)
